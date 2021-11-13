ece4d788a746f6b3f806e6dbee17eec9657ea5ac
brian.orwe <brian.orwe@gmail.com>
2021-11-13 22:45:19 +0300

    Done with Chapter 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String>= env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {}",err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong eading the file");
-
-    println!("With text:\n{}",contents);
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application error: {}",e);
+        process::exit(1);
+    }
 }
